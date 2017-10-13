19674784b6f5616190d7688b105208fa006ae923
Facundo Crusta <facundocrusta@gmail.com>
2017-10-12 21:52:46 -0300

    Facundo Crusta - Agrego ejemplo de Menu de opciones

--- primerProyecto/src/interfaz/main.java
+++ primerProyecto/src/interfaz/main.java
@@ -11,37 +11,41 @@ public class main {
     public static void main(String args[]) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-       Persona persona = new Persona();
-       Persona persona1 = new Persona("Juan", "Gomez", 20, "Argentino");
-       Persona persona2 = new Persona("Efrain", "Kain");
+        Persona persona = new Persona();
+        Persona persona1 = new Persona("Juan", "Gomez", 20, "Argentino");
+        Persona persona2 = new Persona("Efrain", "Kain");
+        int ejecucionPrincipal = 0;
+        int ejecucionPrograma = 0;
+        //Menu Principal
+        do {
+            do {
+                System.out.println("-------- Menu Principal ------");
+                System.out.println("1- Agregar Usuario");
+                System.out.println("2- Ver Usuario");
+                System.out.println("3- Borrar Usuario");
+                System.out.println("------------------------------");
+                //Se solicita la opcion a elegir al usuario
+                System.out.println("Ingrese Opcion: ");
+                int opcionMenu = br.read();
 
+                switch (opcionMenu) {
+                    case 1: {
+                        //Logica para crear un nuevo usuario
+                        break;
+                    }
+                    case 2: {
+                        //Logica para ver un usuario
+                    }
+                    case 3: {
+                        //Logica para borrar un usuario
+                    }
+                    default: {
+                        ejecucionPrincipal = 1;
+                    }
+                }
 
-
-       //Esto se muestra por pantalla
-       System.out.print("Ingrese Nombre: ");
-       String nombre = br.readLine();
-       System.out.print("Ingrese Apellido: ");
-       String apellido = br.readLine();
-
-       //Cambio
-
-       if (nombre.length() >= 3) {
-           persona.setNombre(nombre);
-       } else {
-           persona.setNombre(" ");
-       }
-
-       persona.setApellido(apellido);
-
-       System.out.println("Nombre: " + persona.getNombre());
-       System.out.println("Apellido: " +persona.getApellido());
-
-
-
-
-
-
-
+            }while (ejecucionPrograma < 1);
+        }while (ejecucionPrincipal < 1);
 
     }
 }
