c797ddfd67479ff36bfcd6dc7a3e419c05a593b3
Facundo Crusta <facundocrusta@gmail.com>
2017-10-12 23:13:55 -0300

    Facundo Crusta  - THC-001 - Agrego ciclo principal de ejecucion

--- primerProyecto/src/interfaz/main.java
+++ primerProyecto/src/interfaz/main.java
@@ -1,51 +1,77 @@
 package interfaz;
 
-import fuente.Persona;
-
+import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class main {
 
-
     public static void main(String args[]) throws Exception{
+        //Variables auxiliares y declaraciones de objetos
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        Persona persona = new Persona();
-        Persona persona1 = new Persona("Juan", "Gomez", 20, "Argentino");
-        Persona persona2 = new Persona("Efrain", "Kain");
-        int ejecucionPrincipal = 0;
         int ejecucionPrograma = 0;
-        //Menu Principal
-        do {
+        boolean noEsPrimerCiclo = false;
+        boolean finalizarPrograma = false;
+        //Ciclo de ejecucion
+        while (!finalizarPrograma)
+        {
             do {
+                if (noEsPrimerCiclo) {
+                   limpiarPantalla();
+                }
+                noEsPrimerCiclo = true;
+                //Menu Principal
                 System.out.println("-------- Menu Principal ------");
                 System.out.println("1- Agregar Usuario");
                 System.out.println("2- Ver Usuario");
                 System.out.println("3- Borrar Usuario");
                 System.out.println("------------------------------");
                 //Se solicita la opcion a elegir al usuario
-                System.out.println("Ingrese Opcion: ");
-                int opcionMenu = br.read();
-
+                System.out.print("Ingrese Opcion: ");
+                String opcion = br.readLine();
+                int opcionMenu = Integer.parseInt(opcion);
                 switch (opcionMenu) {
+                    //Agregar Usuario
                     case 1: {
                         //Logica para crear un nuevo usuario
                         break;
                     }
+                    //Ver Usuario
                     case 2: {
                         //Logica para ver un usuario
+                        break;
                     }
+                    //Borrar Usuario
                     case 3: {
                         //Logica para borrar un usuario
+                        break;
                     }
                     default: {
-                        ejecucionPrincipal = 1;
+//                        ejecucionPrograma = 1;
+//                        finalizarPrograma = true;
+                        break;
                     }
                 }
 
             }while (ejecucionPrograma < 1);
-        }while (ejecucionPrincipal < 1);
+        }
 
+    }
+
+//    public static void limpiarPantalla(){
+//        for (int i = 0; i < 10 ; i++) {
+//            System.out.println("");
+//        }
+//    }
+
+    public static void limpiarPantalla() throws AWTException {
+        try {
+            String cmd = "limpiarPantalla";
+            Runtime.getRuntime().exec(cmd);
+        } catch (IOException ioe) {
+            System.out.println (ioe);
+        }
     }
 }
