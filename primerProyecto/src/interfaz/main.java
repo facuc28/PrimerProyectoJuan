16505eae086d5e435e6c19f9de9216e80ddceda5
Facundo Crusta <facundocrusta@gmail.com>
2017-10-13 00:34:43 -0300

    Facundo Crusta - Agrego la logica de las demas funciones y agrego algunas validaciones!

--- primerProyecto/src/interfaz/main.java
+++ primerProyecto/src/interfaz/main.java
@@ -4,6 +4,8 @@ import fuente.Usuario;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class main {
 
@@ -11,6 +13,7 @@ public class main {
         //Variables auxiliares y declaraciones de objetos
         Usuario usuario1 = null;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<Usuario> usuarios = new ArrayList<>();
         String salidaPantalla = "";
         int ejecucionPrograma = 0;
         boolean noEsPrimerCiclo = false;
@@ -20,15 +23,10 @@ public class main {
         {
             do {
                 if (noEsPrimerCiclo) {
-                   limpiarPantalla();
+                    limpiarPantalla();
                 }
                 noEsPrimerCiclo = true;
-                if (!salidaPantalla.equalsIgnoreCase("")) {
-                    System.out.println("------Resultado-------");
-                    System.out.println(salidaPantalla);
-                    System.out.println("----------------------");
-                }
-                salidaPantalla = "";
+
                 //Menu Principal
                 System.out.println("-------- Menu Principal ------");
                 System.out.println("1- Agregar Usuario");
@@ -36,6 +34,14 @@ public class main {
                 System.out.println("3- Borrar Usuario");
                 System.out.println("4- Salir");
                 System.out.println("------------------------------");
+                //Esta condicion checkea si hay algo que mostrar entonces lo muestra.
+                if (!salidaPantalla.equalsIgnoreCase("")) {
+                    System.out.println("------Resultado-------");
+                    System.out.println(salidaPantalla);
+                    System.out.println("----------------------");
+                }
+                //Limpiamos la variable que mostramos en pantalla para mantener esto ordenado y  sin basura
+                salidaPantalla = "";
                 //Se solicita la opcion a elegir al usuario
                 System.out.print("Ingrese Opcion: ");
                 String opcion = br.readLine();
@@ -47,23 +53,34 @@ public class main {
                         System.out.println("Ingrese nombre usuario: ");
                         String usuario = br.readLine();
                         System.out.println("Ingrese contraseña: ");
-                        String contraseña = br.readLine();
+                        String contrasena = br.readLine();
                         System.out.println("Ingrese alias: ");
                         String alias = br.readLine();
-                        usuario1 = new Usuario(usuario, contraseña, alias);
+                        usuario1 = new Usuario(usuario, contrasena, alias);
+                        usuarios.add(usuario1);
+                        salidaPantalla = "Usuario Creado!";
                         break;
                     }
                     //Ver Usuario
                     case 2: {
                         //Logica para ver un usuario
-                        salidaPantalla = usuario1.toString();
+                        System.out.println("Ingrese Nombre usuario: ");
+                        String nombreUsuario =  br.readLine();
+                        Usuario usuarioBuscado = buscarUsuario(nombreUsuario, usuarios);
+                        if (usuarioBuscado != null) {
+                            salidaPantalla = usuarioBuscado.toString();
+                        } else {
+                            salidaPantalla = "Usuario No Encontrado!";
+                        }
                         break;
                     }
                     //Borrar Usuario
                     case 3: {
                         //Logica para borrar un usuario
-                        usuario1 = null;
-                        System.out.println("Usuario Borrado.");
+                        System.out.println("Ingrese nombre de usuario a borrar: ");
+                        String nombreUsuario = br.readLine();
+                        usuarios.remove(buscarUsuario(nombreUsuario, usuarios));
+                        salidaPantalla = "Usuario Borrado!";
                         break;
                     }
                     default: {
@@ -77,10 +94,18 @@ public class main {
         }
 
     }
-    public static void limpiarPantalla(){
+    private static void limpiarPantalla(){
         for (int i = 0; i < 10 ; i++) {
             System.out.println("");
         }
     }
 
+    private static Usuario buscarUsuario(String nombreUsuario, List<Usuario> usuarios) {
+        for (Usuario usuario : usuarios) {
+            if (usuario.getNombre().equalsIgnoreCase(nombreUsuario))
+                return usuario;
+        }
+
+        return null;
+    }
 }
